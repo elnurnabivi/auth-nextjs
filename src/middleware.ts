6f32deb82992c569fe184ec0f8a753eaa2985d6f
elnurnabivi <elnurnabivi@gmail.com>
2024-02-07 17:54:01 +0100

    Added middleware and me route

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+//LOGIC PART
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  //they will not be visible who dont have token
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  //redirect authenticated users to the slash of app. In case they wanna go to signup, login
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+//MATCHING PART
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
